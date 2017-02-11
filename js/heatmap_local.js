301ae6a925728ff3e2e564248efc249cc108a3ff
s-nakano-nichiwa <s-nakano@nichiwa-system.co.jp>
2017-02-11 17:18:00 +0900

    日付表示化と2011/1/1からのデータを追加。

--- js/heatmap_local.js
+++ js/heatmap_local.js
@@ -1,41 +1,57 @@
 $(document).ready(function() {
 
-	// don't forget to add gmaps-heatmap.js
-	var myLatlng = new google.maps.LatLng(34.666104, 135.497401);
+	var myLatlng = new google.maps.LatLng(41.0514, 141.2087);
 	// map options,
 	var myOptions = {
-		zoom: 15,
+		zoom: 5,
+//		zoom: 15,
 		center: myLatlng
 	};
+
 	// standard map
 	map = new google.maps.Map(document.getElementById("map"), myOptions);
 	// heatmap layer
 	heatmap = new HeatmapOverlay(map, 
 	{
-		// radius should be small ONLY if scaleRadius is true (or small radius is intended)
-		"radius": 50,
+		"radius": 30,
+//		"radius": 50,
 	    "maxOpacity": .7, 
-	    // scales the radius based on map zoom
 	    "scaleRadius": false, 
-	    // if set to false the heatmap uses the global maximum for colorization
-	    // if activated: uses the data maximum within the current map boundaries 
-	    //   (there will always be a red spot with useLocalExtremas true)
 	    "useLocalExtrema": false,
-	    // which field name in your data represents the latitude - default "lat"
-	    latField: 'lat',
-	    // which field name in your data represents the longitude - default "lng"
-	    lngField: 'lng',
-	    // which field name in your data represents the data value - default "value"
-	    valueField: 'count'
+	    "latField": 'lat',
+	    "lngField": 'lng',
+	    "valueField": 'count'
 	});
 	
-	var list = ["json/20170113.json", "json/20170114.json"];
+	// フォルダから一気に取得したい
+	var list = [
+	  "json/20110101.json"
+	, "json/20110102.json"
+	, "json/20110103.json"
+	, "json/20110104.json"
+	, "json/20110105.json"
+	, "json/20110106.json"
+	, "json/20110107.json"
+	, "json/20110108.json"
+	, "json/20110109.json"
+	, "json/20110110.json"
+	];
 	var count = 0;
 	setInterval(function() {
-		$.get(list[count++], function(data){
+		$.get(list[count], function(data) {
 			var json = $.parseJSON(data);
+			// 日付取得
+			var date = list[count].split("/")[1].split(".")[0];
+			var ymdStr = date.substr(0,4) + "/";
+			ymdStr += date.substr(4,2) + "/";
+			ymdStr += date.substr(6,2) + "";
+
 			// ヒートマップ反映
 			heatmap.setData(json);
+			$("#date").text(ymdStr);
+			
+			count++;
+			
 			// ループさせる
 			if (count >= list.length) {
 				count = 0;
