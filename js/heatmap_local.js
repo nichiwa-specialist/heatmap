b8340eeec7adda68b3c3d6ec005523805bc96295
s-nakano-nichiwa <s-nakano@nichiwa-system.co.jp>
2017-03-13 18:39:01 +0900

    Update heatmap_local.js

--- js/heatmap_local.js
+++ js/heatmap_local.js
@@ -40,18 +40,17 @@ $(document).ready(function() {
 	setInterval(function() {
 		$.get(list[count], function(data) {
 			console.log(data);
-/*
 			var json = $.parseJSON(data);
 			// 日付取得
 			var date = list[count].split("/")[1].split(".")[0];
 			var ymdStr = date.substr(0,4) + "/";
 			ymdStr += date.substr(4,2) + "/";
 			ymdStr += date.substr(6,2) + "";
-*/
+
 			// ヒートマップ反映
-//			heatmap.setData(json);
-			heatmap.setData(data);
-//			$("#date").text(ymdStr);
+			heatmap.setData(json);
+//			heatmap.setData(data);
+			$("#date").text(ymdStr);
 			
 			count++;
 			
