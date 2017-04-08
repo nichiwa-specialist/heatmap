6b2851e275f64cdbd3413235401410ef48704005
Y Okumura <y-okumura@nichiwa-system.co.jp>
2017-04-08 13:39:35 +0900

    ファイルリストを部ファイルに外出し
    ついでにスピードを調節

--- js/heatmap_local.js
+++ js/heatmap_local.js
@@ -11,51 +11,40 @@ $(document).ready(function() {
 	// standard map
 	map = new google.maps.Map(document.getElementById("map"), myOptions);
 	// heatmap layer
-	heatmap = new HeatmapOverlay(map, 
+	heatmap = new HeatmapOverlay(map,
 	{
 		"radius": 30,
 //		"radius": 50,
-	    "maxOpacity": .7, 
-	    "scaleRadius": false, 
+	    "maxOpacity": .7,
+	    "scaleRadius": false,
 	    "useLocalExtrema": false,
 	    "latField": 'lat',
 	    "lngField": 'lng',
 	    "valueField": 'count'
 	});
-	
-	// フォルダから一気に取得したい
-	var list = [
-	  "json/20110101.txt"
-	, "json/20110102.txt"
-	, "json/20110103.txt"
-	, "json/20110104.txt"
-	, "json/20110105.txt"
-	, "json/20110106.txt"
-	, "json/20110107.txt"
-	, "json/20110108.txt"
-	, "json/20110109.txt"
-	, "json/20110110.txt"
-	];
-	var count = 0;
-	setInterval(function() {
-		$.get(list[count], function(data) {
-			// 日付取得
-			var date = list[count].split("/")[1].split(".")[0];
-			var ymdStr = date.substr(0,4) + "/";
-			ymdStr += date.substr(4,2) + "/";
-			ymdStr += date.substr(6,2) + "";
 
-			// ヒートマップ反映
-			heatmap.setData($.parseJSON(data));
-//			heatmap.setData(data);
-			$("#date").text(ymdStr);
-			
-			count++;
-			
-			// ループさせる
-			if (count >= list.length) {
-				count = 0;
-			}
-		});
-	}, 2000);
+	$.get("json/list.json", function(list) {
+		var count = 0;
+		setInterval(function() {
+			$.get(list[count], function(data) {
+				// 日付取得
+				var date = list[count].split("/")[1].split(".")[0];
+				var ymdStr = date.substr(0,4) + "/";
+				ymdStr += date.substr(4,2) + "/";
+				ymdStr += date.substr(6,2) + "";
+
+				// ヒートマップ反映
+				heatmap.setData($.parseJSON(data));
+				//			heatmap.setData(data);
+				$("#date").text(ymdStr);
+
+				count++;
+
+				// ループさせる
+				if (count >= list.length) {
+					count = 0;
+				}
+			});
+		}, 100);
+	})
 });
